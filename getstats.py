09ffd0f1fd11caf3e9e5b4f392bdcdececc8a162
Zachary Tidwell <123427247+zachtidwell@users.noreply.github.com>
2023-04-06 19:45:34 -0500

    Add files via upload

--- getstats.py
+++ getstats.py
@@ -1,50 +1,73 @@
 import pandas as pd
 from nba_api.stats.static import teams
-from nba_api.stats.endpoints import leaguedashteamstats, leaguestandings, commonteamroster
+from nba_api.stats.endpoints import leaguedashteamstats, commonteamroster, leaguestandings
 import time
+import json
 
 
-# This python script will create 3 json files in the current working directory
-# It will create 'nba-stats.json', 'nba-standings.json', and 'nba_rosters.json'
+# This python script will create 1 .json file in the current working directory
+# It will create 'nba-data.json'
 # Built using 'nba_api' created by Swar Patel
 # https://github.com/swar/nba_api
 
+def get_data():
+    sleep_timer = 0.600
 
-# Get all the teams
-nba_teams = teams.get_teams()
+    # Get all the teams
+    nba_teams = teams.get_teams()
 
-# initialize a list for all the rosters dataframes
-rosters = []
+    # initialize a dictionary for the data
+    data = {}
 
-# Loop through each team and get their season stats, and rosters
-for team in nba_teams:
-    team_id = team['id']
-    team_name = team['full_name']
+    # Loop through each team and get their season stats, roster, and standings
+    for team in nba_teams:
+        team_id = team['id']
+        team_name = team['full_name']
 
-    # Sleeping as to not overload the api
-    time.sleep(.600)
-    # Retrieve team season stats
-    team_stats = leaguedashteamstats.LeagueDashTeamStats(team_id, season='2022-23')
-    team_stats_df = team_stats.get_data_frames()[0]
+        # Sleeping as to not overload the api
+        time.sleep(sleep_timer)
 
-    # Sleeping as to not overload the api
-    time.sleep(.600)
-    # Retrieve team rosters
-    team_rosters = commonteamroster.CommonTeamRoster(team_id, season='2022-23')
-    team_rosters_df = team_rosters.get_data_frames()[0]
-    # Add a column 'TEAM' and fill with team_name
-    team_rosters_df['TEAM'] = team_name
-    # Add each roster to the roster list
-    rosters.append(team_rosters_df)
+        # Retrieve team season stats
+        team_stats = leaguedashteamstats.LeagueDashTeamStats(team_id, season='2022-23')
+        team_stats_df = team_stats.get_data_frames()[0]
+        team_stats_df = team_stats_df[['PTS', 'REB', 'AST', 'STL', 'BLK', 'GP']]
+        team_stats_dict = team_stats_df.to_dict('records')[0]
 
-# Concatenate each roster to the final dataframe
-team_rosters_df = pd.concat(rosters)
+        # Sleeping as to not overload the api
+        time.sleep(sleep_timer)
 
-# Retrieve the league standings of each team
-team_standings = leaguestandings.LeagueStandings(season='2022-23')
-team_standings_df = team_standings.get_data_frames()[0]
+        # Retrieve team roster
+        team_roster = commonteamroster.CommonTeamRoster(team_id, season='2022-23')
+        team_roster_df = team_roster.get_data_frames()[0]
+        team_roster_df = team_roster_df[['PLAYER', 'POSITION', 'NUM', 'HEIGHT', 'WEIGHT', 'AGE', 'BIRTH_DATE', 'SCHOOL']]
+        team_roster_dict = team_roster_df.to_dict('records')
 
-# Converting each dataframe into a .json file and sending to current directory
-team_stats_df.to_json('nba-stats.json', orient='records')
-team_standings_df.to_json('nba-standings.json', orient='records')
-team_rosters_df.to_json('nba-rosters.json', orient='records')
+        # Sleeping as to not overload the api
+        time.sleep(sleep_timer)
+
+        # Retrieve team standings
+        team_standings = leaguestandings.LeagueStandings(season='2022-23')
+        team_standings_df = team_standings.get_data_frames()[0]
+        team_standings_df = team_standings_df.loc[team_standings_df['TeamID'] == team_id]
+        team_standings_df = team_standings_df[['Conference', 'PlayoffRank', 'ConferenceRecord', 'ConferenceGamesBack', 'L10', 'strCurrentStreak']]
+        team_standings_dict = team_standings_df.to_dict('records')[0]
+
+        team_dict = {
+            'stats': team_stats_dict,
+            'roster': team_roster_dict,
+            'standings': team_standings_dict
+        }
+        data[team_name] = team_dict
+
+
+    # Write data to json file
+    with open('nba-data.json', 'w') as f:
+        json.dump(data, f)
+
+
+def main():
+    get_data()
+
+
+if __name__ == "__main__":
+    main()
